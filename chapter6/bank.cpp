af2c02a124a169ea4dea8a384b17d87d00bcf19b
Alexandru Bolboaca <alexboly@gmail.com>
2018-12-15 12:20:15 +0200

    Implement series-based account id. Use curry to remove duplication.

--- chapter6/bank.cpp
+++ chapter6/bank.cpp
@@ -6,14 +6,25 @@
 using namespace std;
 using namespace std::placeholders;
 
-int createAccount(string accountOwnerName){
-    return 1;
+auto createAccount = [](string accountOwnerName,  int lastCount){
+    return lastCount + 1;
+};
+
+auto createFirstAccount = bind(createAccount, _1, 0);
+
+TEST_CASE("Adds values"){
+    string accountOwnerName = "Alex";
+
+    int accountId = createFirstAccount(accountOwnerName);
+
+    CHECK_EQ(1, accountId);
 }
 
 TEST_CASE("Adds values"){
     string accountOwnerName = "Alex";
 
-    int accountId = createAccount(accountOwnerName);
+    int firstAccountId = createFirstAccount(accountOwnerName);
+    int accountId = createAccount(accountOwnerName, firstAccountId);
 
-    CHECK_EQ(1, accountId);
+    CHECK_EQ(2, accountId);
 }
