4d75fcbc1f84172818cbd2ed55d333874adcdba8
Alexandru Bolboaca <alexboly@gmail.com>
2019-01-08 17:52:06 +0200

    Working version.
    Still need to remove some of the types.

--- chapter6/ticTacToeResult.cpp
+++ chapter6/ticTacToeResult.cpp
@@ -1,42 +1,101 @@
 #include <iostream>
 #include <functional>
+#include <numeric>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
 using namespace std;
 using namespace std::placeholders;
 
-typedef vector<vector<char>> Board;
+typedef vector<char> Line;
+typedef vector<Line> Board;
 
-auto all_lines = [](auto const board) {
-    return board;
+template<typename SourceType, typename DestinationType = SourceType>
+auto transformAll = [](SourceType source,  auto lambda){
+    DestinationType result;
+    transform(source.begin(), source.end(), back_inserter(result), lambda);
+    return result;
 };
 
-auto all_columns = [](auto const board) {
-    vector<vector<char>> result(board[0].size(), vector<char>(board.size()));
-    for (vector<char>::size_type i = 0; i < board[0].size(); i++) 
-        for (vector<char>::size_type j = 0; j < board.size(); j++) {
-            result[i][j] = board[j][i];
-        }
-    return result;
+auto lineToString = [](const auto line){
+    return transformAll<Line, string>(line, [](auto token) -> char { return token;});
+};
+
+auto boardToLinesString = [](const auto board){
+    return transformAll<Board, vector<string>>(board, [](Line line) { return lineToString(line); });
+};
+
+auto boardToString = [](auto const board){
+    auto linesAsString = boardToLinesString(board);
+    return accumulate(linesAsString.begin(), linesAsString.end(), string(), 
+            [](string current, string lineAsString) { return current + lineAsString + "\n"; }
+    );
+};
+
+auto concatenate = [](auto first, auto const second){
+    first.insert(first.end(), make_move_iterator(second.begin()), make_move_iterator(second.end()));
+    return first;
+};
+
+auto toRange = [](auto const board){
+    vector<int> range(board.size());
+    iota(begin(range), end(range), 0);
+    return range;
+};
+
+auto identical = [](auto const value){return value;};
+
+auto all_lines = [](const auto board) {
+    return transformAll<Board>(board, identical); 
+};
+
+auto column = [](const auto board, int index){
+    auto range = toRange(board);
+    return transformAll<vector<int>, Line>(range, [&](auto i) { return board[i][index]; });
+};
+
+auto mainDiagonal = [](const auto board){
+    Line diagonal;
+    auto range = toRange(board);
+    return transformAll<vector<int>, Line>(range, [&](auto index){ return board[index][index];});
+};
+
+auto secondaryDiagonal = [](const auto board){
+    Line diagonal;
+    auto range = toRange(board);
+    return transformAll<vector<int>, Line>(range, [&](auto index){ return board[index][board.size() - index - 1];});
+};
+
+auto all_columns = [](const Board& board) {
+    Board result;
+    auto range = toRange(board); 
+    auto columnForBoardAndIndex = bind(column, board, _1);
+    return transformAll<vector<int>, Board>(range, columnForBoardAndIndex);
 };
 
 auto all_diagonals = [] (auto const board){
-    vector<vector<char>> result(2, vector<char>(board.size()));
+    return vector<Line>( {mainDiagonal(board), secondaryDiagonal(board)});
+};
 
-    for(int i = 0; i < board.size(); ++i){
-        result[0][i] = board[i][i];
-    }
+auto allLinesColumnsAndDiagonals = [](auto const board){
+    Board all;
+    all = concatenate(all, all_lines(board));
+    all = concatenate(all, all_columns(board));
+    all = concatenate(all, all_diagonals(board));
+    return all;
+};
 
-    for(int i = 0; i < board.size(); ++i){
-        result[1][i] = board[i][board.size() - i - 1];
-    }
+auto lineFilledWithX = [](auto const line){
+    return all_of(line.begin(), line.end(), [](auto token){ return token == 'X';});
+};
 
-    return result;
+auto xWins = [](auto const board){
+    auto all = allLinesColumnsAndDiagonals(board);
+    return any_of(all.begin(), all.end(), [](auto line){ return lineFilledWithX(line);});
 };
 
 TEST_CASE("all lines"){
-    vector<vector<char>> board = {
+    Board board = {
         {'X', 'X', 'X'},
         {' ', 'O', ' '},
         {' ', ' ', 'O'}
@@ -46,13 +105,13 @@ TEST_CASE("all lines"){
 }
 
 TEST_CASE("all columns"){
-    vector<vector<char>> board = {
+    Board board = {
         {'X', 'X', 'X'},
         {' ', 'O', ' '},
         {' ', ' ', 'O'}
     };
 
-    vector<vector<char>> expectedColumns = {
+    Board expectedColumns = {
         {'X', ' ', ' '},
         {'X', 'O', ' '},
         {'X', ' ', 'O'},
@@ -74,4 +133,91 @@ TEST_CASE("all diagonals"){
     };
 
     CHECK_EQ(expectedDiagonals, all_diagonals(board));
+}
+
+TEST_CASE("all lines, columns and diagonals"){
+    vector<vector<char>> board = {
+        {'X', 'X', 'X'},
+        {' ', 'O', ' '},
+        {' ', ' ', 'O'}
+    };
+
+    vector<vector<char>> expected = {
+        {'X', 'X', 'X'},
+        {' ', 'O', ' '},
+        {' ', ' ', 'O'},
+        {'X', ' ', ' '},
+        {'X', 'O', ' '},
+        {'X', ' ', 'O'},
+        {'X', 'O', 'O'},
+        {'X', 'O', ' '}
+    };
+
+    auto all = allLinesColumnsAndDiagonals(board);
+    CHECK_EQ(expected, all);
+}
+
+TEST_CASE("line to string"){
+    vector<char> line = {
+        ' ', 'X', 'O'
+    };
+
+    CHECK_EQ(" XO", lineToString(line));
+}
+
+TEST_CASE("board to lines string"){
+    vector<vector<char>> board = {
+        {'X', 'X', 'X'},
+        {' ', 'O', ' '},
+        {' ', ' ', 'O'}
+    };
+    vector<string> expected = {
+        "XXX",
+        " O ",
+        "  O"
+    };
+
+    CHECK_EQ(expected, boardToLinesString(board));
+}
+
+TEST_CASE("board to string"){
+    vector<vector<char>> board = {
+        {'X', 'X', 'X'},
+        {' ', 'O', ' '},
+        {' ', ' ', 'O'}
+    };
+    string expected = "XXX\n O \n  O\n";
+
+    CHECK_EQ(expected, boardToString(board));
+}
+
+TEST_CASE("Line filled with X"){
+    vector<char> line = {'X', 'X', 'X'};
+
+    CHECK(lineFilledWithX(line));
+}
+
+TEST_CASE("X wins"){
+    vector<vector<char>> board = {
+        {'X', 'X', 'X'},
+        {' ', 'O', ' '},
+        {' ', ' ', 'O'}
+    };
+
+    CHECK(xWins(board));
+}
+
+TEST_CASE("Project column"){
+    vector<vector<char>> board = {
+        {'X', 'X', 'X'},
+        {' ', 'O', ' '},
+        {' ', ' ', 'O'}
+    };
+
+    vector<char> expected0 = {'X', ' ', ' '};
+    CHECK_EQ(expected0, column(board, 0));
+    vector<char> expected1 = {'X', 'O', ' '};
+    CHECK_EQ(expected1, column(board, 1));
+    vector<char> expected2 = {'X', ' ', 'O'};
+    CHECK_EQ(expected2, column(board, 2));
 }
