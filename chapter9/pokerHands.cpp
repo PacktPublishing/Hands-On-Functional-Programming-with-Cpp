de580d51a36aee09dfe89d8ab2ee2e5e59fd129c
Alexandru Bolboaca <alexboly@gmail.com>
2019-03-17 11:52:33 +0200

    Failing test: Alice wins with straight flush.

--- chapter9/pokerHands.cpp
+++ chapter9/pokerHands.cpp
@@ -7,6 +7,22 @@
 using namespace std;
 using namespace std::placeholders;
 
-TEST_CASE("Pass"){
-    CHECK(true);
+/*
+Case: Alice wins
+
+Inputs:
+    Alice: 2♠, 3♠, 4♠, 5♠, 6♠
+    Bob: 2♣, 4♦, 7♥, 9♠, A♥
+
+Output:
+    Alice wins with straight flush
+*/
+
+TEST_CASE("Alice wins with straight flush"){
+    auto aliceHand = {"2♠", "3♠", "4♠", "5♠", "6♠"};
+    auto bobHand = {"2♣", "4♦", "7♥", "9♠", "A♥"};
+
+    auto result = comparePokerHands(aliceHand, bobHand);
+
+    CHECK_EQ("Alice wins with straight flush", result);
 }
