da6c52c76ff8874233f62847a172beef3a68df7c
Alexandru Bolboaca <alexboly@gmail.com>
2019-04-08 21:23:36 +0300

    Base for chapter11 code

---
+++ chapter11/performance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <numeric>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+using namespace std;
+using namespace std::placeholders;
+
+TEST_CASE("Test"){
+    CHECK_EQ(true, false);
+}
