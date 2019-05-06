a252239eeff797829ee2eb1bb64dc1217080df6a
Alexandru Bolboaca <alexboly@gmail.com>
2019-05-06 15:23:25 +0300

    Last property passes

--- chapter12/propertyBasedTests.cpp
+++ chapter12/propertyBasedTests.cpp
@@ -29,6 +29,17 @@ auto power = [](int first, int second){
 };
 
 auto generate_ints = [](int min, int max){
+    if(min > max) {
+        return vector<int>();
+    }
+    if(min == max){
+        return range(min, min);
+    }
+
+    if(max - min <= 100){
+        return range(min, max);
+    }
+
     random_device rd;
     mt19937 generator(rd());
     uniform_int_distribution<int> distribution(min, max);
@@ -43,13 +54,15 @@ auto generate_ints = [](int min, int max){
 
 auto logMaxIntBaseX = [](int x) -> int{
     auto maxInt = numeric_limits<int>::max() ;
-    return (int)floor(log(maxInt) / log(x));
+    return floor(log(maxInt) / log(x));
 };
 
 auto generate_ints_greater_than_1 = bind(generate_ints, 1, numeric_limits<int>::max());
 auto generate_ints_greater_than_0 = bind(generate_ints, 0, numeric_limits<int>::max());
+auto generate_ints_greater_than_2_less_sqrt_maxInt = bind(generate_ints, 2, sqrt(numeric_limits<int>::max()));
+
 auto generate_exponent_less_than_log_maxInt = [](auto x){
-    return generate_ints(x, logMaxIntBaseX(x));
+    return generate_ints(1, logMaxIntBaseX(x));
 };
 
 auto all_of_collection = [](auto collection, auto lambda){
@@ -65,7 +78,9 @@ auto printGeneratedValues = [](const string& generatorName, const auto& values){
 auto check_property = [](auto generator, auto property, string generatorName){
     auto values = generator();
     printGeneratedValues(generatorName, values);
-    CHECK(all_of_collection(values, property));
+    bool pass = all_of_collection(values, property);
+    CHECK(pass);
+    return pass;
 };
 
 auto property_0_to_power_0_is_1 = [](){
@@ -88,9 +103,8 @@ auto prop_any_int_to_power_1_is_the_value = [](int base){
 
 auto prop_nextPowerOfXIsPreviousPowerOfXMultipliedByX = [](int x){
     auto exponents = bind(generate_exponent_less_than_log_maxInt, x);
-    check_property(exponents, [x](auto y){ return power(x, y) == power(x, y - 1) * x;}, "generate exponents");
+    return check_property(exponents, [x](auto y){ return power(x, y) == power(x, y - 1) * x;}, "generate exponents for " + to_string(x));
 };
-
 
 TEST_CASE("Properties"){
     cout << "Property: 0 to power 0 is 1" << endl;
@@ -105,6 +119,6 @@ TEST_CASE("Properties"){
     cout << "Property: any int to power 1 is the value" << endl;
     check_property(generate_ints_greater_than_0, prop_any_int_to_power_1_is_the_value, "generate ints");
 
-//    cout << "Property: next power of x is previous power of x multiplied by x" << endl;
-//    check_property(generate_ints_greater_than_1, prop_nextPowerOfXIsPreviousPowerOfXMultipliedByX, "generate ints");
+    cout << "Property: next power of x is previous power of x multiplied by x" << endl;
+    check_property(generate_ints_greater_than_2_less_sqrt_maxInt, prop_nextPowerOfXIsPreviousPowerOfXMultipliedByX, "generate greater than 2 and less than sqrt of maxInt");
 }
