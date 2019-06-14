9f17aaeb0c22c4a59dda2ea8f259bbf5cc43e701
Alexandru Bolboaca <alexboly@gmail.com>
2019-06-14 15:02:02 +0300

    More const refs

--- Chapter10/reactive.cpp
+++ Chapter10/reactive.cpp
@@ -11,9 +11,9 @@ using namespace std::placeholders;
 using namespace std::chrono;
 
 template<typename DestinationType>
-auto transformAll = [](const auto& source, auto lambda){
+auto transformAll = [](const auto& source, const auto& fn){
     DestinationType result(source.size());
-    transform(source.begin(), source.end(), result.begin(), lambda);
+    transform(source.begin(), source.end(), result.begin(), fn);
     return result;
 };
 
@@ -27,15 +27,15 @@ auto rangeFrom1To = bind(rangeFromTo, 1, _1);
 
 auto rangeFrom2To = bind(rangeFromTo, 2, _1);
 
-auto isDivisibleBy = [](auto value, auto factor){
+auto isDivisibleBy = [](const int value, const int factor){
     return (value % factor == 0); 
 };
 
-auto none_of_collection = [](auto aCollection, auto lambda){
-    return none_of(aCollection.begin(), aCollection.end(), lambda);
+auto none_of_collection = [](const auto& aCollection, const auto& fn){
+    return none_of(aCollection.begin(), aCollection.end(), fn);
 };
 
-auto is_prime = [](int x) {
+auto is_prime = [](const int x) {
     auto xIsDivisibleBy = bind(isDivisibleBy, x, _1);
     return none_of_collection(
             rangeFrom2To(x - 1), 
@@ -43,7 +43,7 @@ auto is_prime = [](int x) {
         );
 };
 
-auto printIsPrime = [](int value){
+auto printIsPrime = [](const int value){
     cout << value << (is_prime(value) ? " is prime" : " is not prime") << endl;
 };
 
